ddd165d3876471ea117a7def6f433a81b2e093c2
Brian <speed.bran@hotmail.com>
2020-03-15 11:44:59 -0300

    [Experimental] added stuncrit
    Instead of displaying "(Critical hit!) (Stun hit!)" if both, it should in theory now display "(Critical stun hit!)" if nothing explodes

--- src/Drawings/FightManager.js
+++ src/Drawings/FightManager.js
@@ -53,19 +53,25 @@ class FightManager {
         let ind = fight.summaryIndex;
         let summary = fight.summary;
         if (ind < summary.rounds.length) {
-
+            
+            let hitText = "";
+            if (summary.rounds[ind].critical === true && summary.rounds[ind].stun === true) {
+                hitText = " (" + Translator.getString(lang, "fight_general", "critstun_hit") + "!) ";
+            } else if (summary.rounds[ind].critical === true) {
+                hitText = " (" + Translator.getString(lang, "fight_general", "critical_hit") + "!) ";
+            } else if (summary.rounds[ind].stun === true) {
+                hitText = " (" + Translator.getString(lang, "fight_general", "stun_hit") + "!) ";
+            }
+            
             if (summary.rounds[ind].roundType == "Character") {
                 fight = this.swapArrayIndexes("<:user:403148210295537664> " + Translator.getString(lang, "fight_pve", "onfight_user_attack", [summary.rounds[ind].attackerName, summary.rounds[ind].defenderName, summary.rounds[ind].damage]) +
-                    (summary.rounds[ind].critical === true ? " (" + Translator.getString(lang, "fight_general", "critical_hit") + "!) " : "") +
-                    (summary.rounds[ind].stun === true ? " (" + Translator.getString(lang, "fight_general", "stun_hit") + "!) " : "") +
+                    hitText +
                     "\n\n", fight);
             } else if (summary.rounds[ind].roundType == "Monster") {
                 fight = this.swapArrayIndexes("<:monstre:403149357387350016> " + Translator.getString(lang, "fight_pve", "onfight_monster_attack", [summary.rounds[ind].attackerName, summary.rounds[ind].defenderName, summary.rounds[ind].damage]) +
-                    (summary.rounds[ind].critical === true ? " (" + Translator.getString(lang, "fight_general", "critical_hit") + "!) " : "") +
-                    (summary.rounds[ind].stun === true ? " (" + Translator.getString(lang, "fight_general", "stun_hit") + "!) " : "") +
+                    hitText +
                     "\n\n", fight);
             }
-
 
 
             message.edit(this.embedPvE(fight.text[0] + fight.text[1] + fight.text[2], fight, null, lang))
