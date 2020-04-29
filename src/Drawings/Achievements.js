df3a7e16bf372720eb79aff4d3ad8cb29b13b6a8
Jean-Charles TECHER - Desktop <9269672+Roncarlos@users.noreply.github.com>
2020-04-29 17:24:06 +0200

    little visual enhancement

--- src/Drawings/Achievements.js
+++ src/Drawings/Achievements.js
@@ -9,8 +9,11 @@ class Achievements {
             .setColor([0, 255, 0])
             .setAuthor(Translator.getString(data.lang, "character", "achievement_title", [data.totalAchievementsEarned, data.totalAchievements, data.totalPoints]) + " | " + Translator.getString(data.lang, "general", "page_out_of_x", [data.page, data.maxPage]));
 
+
         for (let achievement of data.achievements) {
-            rembed.addField(Translator.getString(data.lang, "character", "achievement_name", [achievement.nameAchievement, achievement.points]) + " - " + (achievement.earned == 1 ? Translator.getString(data.lang, "character", "achievement_earned_word") + " " + Emojis.getString("g_vmark") : Emojis.getString("g_xmark")), (achievement.descAchievement != null ? "```" + achievement.descAchievement + "```" : "```" + Translator.getString(data.lang, "character", "no_desc") + "```"));
+            let status = (achievement.earned == 1 ? Emojis.getString("g_vmark") + " " + Translator.getString(data.lang, "character", "achievement_earned_word") : Emojis.getString("g_xmark"));
+
+            rembed.addField(status + " - " + Emojis.emojisProd.win.string + " " + Translator.getString(data.lang, "character", "achievement_name", [achievement.nameAchievement, achievement.points]), (achievement.descAchievement != null ? "```" + achievement.descAchievement + "```" : "```" + Translator.getString(data.lang, "character", "no_desc") + "```"));
         }
         return rembed;
     }
