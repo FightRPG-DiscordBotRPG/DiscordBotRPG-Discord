1559584817acadcb6b9e77b47038fce464dbd1cd
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2020-12-02 21:14:20 +0100

    using create pool for mysql

--- conf/mysql.js
+++ conf/mysql.js
@@ -3,18 +3,14 @@ var MySql = require('mysql');
 const util = require("util");
 const conf = require("./conf");
 
-var connection = MySql.createConnection(conf.database);
+var connection = MySql.createPool(conf.database);
 
 var query = util.promisify(connection.query).bind(connection);
 let connMaker = {
     connection: connection,
     query: async (sql, arr = []) => {
-        try {
-            const result = await query(sql, arr);
-            return JSON.parse(JSON.stringify(result));
-        } catch (err) {
-            throw err;
-        }
+        const result = await query(sql, arr);
+        return JSON.parse(JSON.stringify(result));
     }
 };
 
--- conf/version.js
+++ conf/version.js
@@ -1,1 +1,1 @@
-module.exports = "1.9.12"
+module.exports = "1.9.13"
