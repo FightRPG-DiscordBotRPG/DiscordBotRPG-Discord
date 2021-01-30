593ddf81c35b4b54877de7bd77c0699e7a60b3d6
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2021-01-30 20:28:55 +0100

    tweeking again number of messages before check

--- src/AntiSpam/UserChallenge.js
+++ src/AntiSpam/UserChallenge.js
@@ -96,7 +96,7 @@ class UserChallenge {
     }
 
     updateLengthCheck() {
-        this.lengthToCheck = Math.round(75 + (Math.random() * 250));
+        this.lengthToCheck = Math.round(100 + (Math.random() * 300));
     }
 }
 
