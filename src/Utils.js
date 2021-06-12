554c82ad962ac3c669accb02cd19cd5a89a88ff5
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2021-06-12 14:21:34 +0200

    fix bad rotation draw

--- src/Utils.js
+++ src/Utils.js
@@ -240,15 +240,21 @@ class Utils {
         }
 
 
-        const canvas = Canvas.createCanvas(image.width, image.height);
+        const canvas = Canvas.createCanvas(width, height);
         const context = canvas.getContext("2d");
 
         context.save();
         context.translate(width / 2, height / 2);
         context.rotate(deg * Math.PI / 180);
         context.translate(-width / 2, -height / 2);
-        context.drawImage(image, 0, 0);
-        context.restore();
+        context.drawImage(
+            image,
+            (width / 2) - (image.width / 2),
+            (height / 2) - (image.height / 2),
+            image.width,
+            image.height);
+
+        //context.restore();
 
         return context.canvas;
     }
