39e60bd140da697e6740830ff86de4bb286e81c7
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2021-06-12 11:37:03 +0200

    added multiple gen possible

--- scripts/Test2D.js
+++ scripts/Test2D.js
@@ -7,7 +7,21 @@ async function start() {
     let appearance = new CharacterAppearance();
     await appearance.debugLoadAssets();
     fs.writeFileSync("./test.png", await (await appearance.getCharacter()).toBuffer());
+
     open("./test.png");
 }
 
-start();
+async function multipleTest() {
+    for (let i = 0; i <= 17; i++) {
+
+        let debugFacialHair = i.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: true });
+        let appearance = new CharacterAppearance();
+        await appearance.debugLoadAssets();
+        appearance.facialHair = await CharacterAppearance.getImage(`W:\\DocumentsWndows\\FightRPG\\character\\Base\\Facial Hair\\${debugFacialHair}.png`);
+
+        fs.writeFileSync("./test" + debugFacialHair + ".png", await (await appearance.getCharacter()).toBuffer());
+    }
+}
+
+//start();
+multipleTest();
