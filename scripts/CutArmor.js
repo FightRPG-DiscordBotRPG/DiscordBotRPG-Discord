fc1d020303affc9555e73485b94625c85628582a
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2021-07-15 14:43:41 +0200

    add test to be sure it's only the needed one

--- scripts/CutArmor.js
+++ scripts/CutArmor.js
@@ -21,12 +21,16 @@ async function start() {
         let fileName = file.replace(".png", "");
 
         //console.log(Utils.canvasCut(img, 0, 0, 350, 512).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_body.png`, Utils.canvasCut(img, 0, 0, 350, 512).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_neck.png`, Utils.canvasCut(img, 350, 0, 190, 177).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_upper_left.png`, Utils.canvasCut(img, 350, 177, 190, 335).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_upper_right.png`, Utils.canvasCut(img, 540, 177, 190, 335).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_lower_left.png`, Utils.canvasCut(img, 730, 232, 147, 280).toBuffer());
-        fs.writeFileSync(`${path}/../${fileName}_lower_right.png`, Utils.canvasCut(img, 877, 232, 147, 280).toBuffer());
+        if (img.width >= 1024) {
+            fs.writeFileSync(`${path}/../${fileName}_body.png`, Utils.canvasCut(img, 0, 0, 350, 512).toBuffer());
+            fs.writeFileSync(`${path}/../${fileName}_neck.png`, Utils.canvasCut(img, 350, 0, 190, 177).toBuffer());
+            fs.writeFileSync(`${path}/../${fileName}_upper_left.png`, Utils.canvasCut(img, 350, 177, 190, 335).toBuffer());
+            fs.writeFileSync(`${path}/../${fileName}_upper_right.png`, Utils.canvasCut(img, 540, 177, 190, 335).toBuffer());
+            fs.writeFileSync(`${path}/../${fileName}_lower_left.png`, Utils.canvasCut(img, 730, 232, 147, 280).toBuffer());
+            fs.writeFileSync(`${path}/../${fileName}_lower_right.png`, Utils.canvasCut(img, 877, 232, 147, 280).toBuffer());
+        }
+
+
     }
 
     //fs.writeFileSync("./testAction.png", );
