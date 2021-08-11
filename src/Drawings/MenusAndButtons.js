9af2e55cb2fd146301b1686ddf8758584d3f9ced
Roncarlos <Roncarlos@DESKTOP-US27VUH>
2021-08-11 19:12:14 +0200

    added class for buttons

---
+++ src/Drawings/MenusAndButtons.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const Translator = require("../Translator/Translator");
+const Emojis = require("./Emojis");
+
+class MenusAndButtons {
+    static getConfirmButton(lang="en") {
+        return new Discord.MessageButton()
+            .setCustomId("confirm")
+            .setLabel(Translator.getString(lang, "general", "yes"))
+            .setStyle("PRIMARY")
+            .setEmoji(Emojis.general.vmark);
+    }
+
+    static getValidateButton(lang = "en") {
+        return new Discord.MessageButton()
+            .setCustomId("confirm")
+            .setLabel(Translator.getString(lang, "general", "validate"))
+            .setStyle("PRIMARY")
+            .setEmoji(Emojis.general.vmark);
+    }
+
+    static getCancelButton(lang="en") {
+       return new Discord.MessageButton()
+            .setCustomId("cancel")
+            .setLabel(Translator.getString(lang, "general", "no"))
+            .setStyle("DANGER")
+            .setEmoji(Emojis.general.xmark)
+    }
+}
+
+
+module.exports = MenusAndButtons;
